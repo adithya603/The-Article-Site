25a2c1f114fdc49a0d9ff8dc23fbc90e482e0ad9
adithya603 <adithya632003@gmail.com>
2024-04-04 00:13:53 +0530

    add JWT cookie

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,6 @@
 import {db} from "../db.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const signup = (req, res)=>{
     //CHECK EXISTING USER
@@ -10,7 +11,7 @@ export const signup = (req, res)=>{
         if(err) return res.json(err)
         if(data.length) return res.status(409).json("user already exists!") //if the user exists already
 
-        //to not store the password as a text, we a module bcrypt that produces the hash code for the password
+        //to not store the password as a text, we use a module bcrypt that produces the hash code for the password
         //npm i bcryptjs
 
         const salt = bcrypt.genSaltSync(10);
@@ -33,7 +34,7 @@ export const login = (req, res)=>{
 
     const q = "SELECT * FROM users WHERE username = ?"
 
-    db.query(q, [req, body, username], (err,data) => {
+    db.query(q, [req.body.username], (err,data) => {
         if(err) return res.json(err)
         if(data.length === 0) return res.status(404).json("User not found!")
 
@@ -42,7 +43,15 @@ export const login = (req, res)=>{
 
         if(!isPasswordCorrect) return res.status(400).json("Incorrect Password")
 
-        
+        //We use JWT(json web token) to verify the user that he's the owner of the post
+        //npm i jsonwebtoken
+
+        const token = jwt.sign({id: data[0].id}, "SomeRandomJWTkey")
+
+        const {password, ...other} = data[0] //seperating password
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(other)
 
     })
 }
--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ import postRoutes from "./routes/posts.js"
 import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/authentication.js"
 import cors from "cors"
+import cookieParser from "cookie-parser";
 
+//npm i express
+//npm i cors
+//npm i cookie-parser
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/posts", postRoutes)
-app.use("/api/users", userRoutes)
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
 
 app.listen(8800, function(){
     console.log("Server has started")
