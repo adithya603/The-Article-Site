1eb6f8d6970c3d120c08c2b6c9cfb5a95e2df78c
adithya603 <adithya632003@gmail.com>
2024-04-09 14:01:24 +0530

    Final base code push before designing

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -73,9 +73,9 @@ export const updatePost = (req, res) => {
     if (err) return res.status(403).json("Token is not valid!");
 
     const postId = req.params.id;
-    const q = "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+    const q = "UPDATE posts SET `title`=?,`desc`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
 
-    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+    const values = [req.body.title, req.body.desc, req.body.cat];
 
     db.query(q, [...values, postId, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
