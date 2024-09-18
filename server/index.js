a019a45e59135988d1eba4adfa5bde44768984a0
adithya603 <adithya632003@gmail.com>
2024-09-19 00:26:27 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ const port = process.env.PORT || 8800;
 const app = express()
 
 //middlewares
-app.use(cors({
-    origin: 'http://localhost:3000', // Change this to your frontend URL in production
-    credentials: true, // Allow credentials (cookies)
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000', // Change this to your frontend URL in production
+//     credentials: true, // Allow credentials (cookies)
+// }));
 app.use(express.json())
 app.use(cookieParser())
 
