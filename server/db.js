8e527b2eb00f4cb9dd8c13f8b20af1ba274c5747
adithya603 <adithya632003@gmail.com>
2024-09-18 15:05:08 +0530

    pushing

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import mysql from "mysql"
 import dotenv from 'dotenv';
 dotenv.config();
@@ -8,3 +9,15 @@ export const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
+=======
+import mysql from "mysql"
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+>>>>>>> a106aadeb1154f1c05a5de2d76d443b0d1876e3a
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import  express  from "express";
 import postRoutes from "./routes/posts.js"
 import authRoutes from "./routes/authentication.js"
@@ -22,3 +23,29 @@ app.use("/api/posts", postRoutes)
 app.listen(port, function(){
     console.log("Server has started on port " + port);
 });
+=======
+import  express  from "express";
+import postRoutes from "./routes/posts.js"
+import authRoutes from "./routes/authentication.js"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 8800;
+
+const app = express()
+
+//middlewares
+app.use(cors())  //works without cors
+app.use(express.json())
+app.use(cookieParser())
+
+//routes
+app.use("/api/auth", authRoutes)
+app.use("/api/posts", postRoutes)
+
+app.listen(port, function(){
+    console.log("Server has started on port " + port);
+});
+>>>>>>> a106aadeb1154f1c05a5de2d76d443b0d1876e3a
