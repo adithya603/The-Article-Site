0251de056f8ec1af0e01691357288f0799755665
adithya603 <adithya632003@gmail.com>
2024-09-18 15:06:27 +0530

    little adjustments

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import  express  from "express";
 import postRoutes from "./routes/posts.js"
 import authRoutes from "./routes/authentication.js"
@@ -23,29 +22,3 @@ app.use("/api/posts", postRoutes)
 app.listen(port, function(){
     console.log("Server has started on port " + port);
 });
-=======
-import  express  from "express";
-import postRoutes from "./routes/posts.js"
-import authRoutes from "./routes/authentication.js"
-import cors from "cors"
-import cookieParser from "cookie-parser";
-import dotenv from 'dotenv';
-dotenv.config();
-
-const port = process.env.PORT || 8800;
-
-const app = express()
-
-//middlewares
-app.use(cors())  //works without cors
-app.use(express.json())
-app.use(cookieParser())
-
-//routes
-app.use("/api/auth", authRoutes)
-app.use("/api/posts", postRoutes)
-
-app.listen(port, function(){
-    console.log("Server has started on port " + port);
-});
->>>>>>> a106aadeb1154f1c05a5de2d76d443b0d1876e3a
