5f62cff8d44e7bfcff0db31b53edf15f21f35b90
adithya603 <adithya632003@gmail.com>
2024-09-18 15:58:08 +0530

    deploy vercel

--- server/db.js
+++ server/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql from "mysql"
 import dotenv from 'dotenv';
 dotenv.config();
@@ -9,15 +8,3 @@ export const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
-=======
-import mysql from "mysql"
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
->>>>>>> a106aadeb1154f1c05a5de2d76d443b0d1876e3a
